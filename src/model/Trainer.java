6b9b7e1215472616c2b70f5c4237a7f6782f0b1c
Saymon Oliveira de Castro <saymon_castro@estudante.sesisenai.org.br>
2025-04-01 18:12:59 -0300

    Insert Club

--- src/model/Trainer.java
+++ src/model/Trainer.java
@@ -8,4 +8,56 @@ public class Trainer {
     private int experience;
     private String name;
     private Club club;
+
+    public Trainer(int idTrainer, int experience, String name, Club club) {
+        this.idTrainer = idTrainer;
+        this.experience = experience;
+        this.name = name;
+        this.club = club;
+    }
+
+    public Trainer(int experience, String name, Club club) {
+        this.experience = experience;
+        this.name = name;
+        this.club = club;
+    }
+
+    public int getIdTrainer() {
+        return idTrainer;
+    }
+
+    public void setIdTrainer(int idTrainer) {
+        this.idTrainer = idTrainer;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+
+    public String toString () {
+        return "\nID do treinador: " +
+               "\nExperiência do treinador: " +
+               "\nNome do treinador: " +
+               "\nClube do treinador: ";
+    }
 }
--- src/service/ClubService.java
+++ src/service/ClubService.java
@@ -1,10 +1,28 @@
 package service;
 import model.Club;
-import Connection.BankConnection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import connection.BankConnection;
+
+import java.sql.*;
 
 public class ClubService {
-    
+
+    public static Club saveClub(Club club) throws SQLException {
+        String sql = "INSERT INTO clube (name, dateFoundation, trainer) VALUES (?,?,?)";
+
+        try (Connection connection = BankConnection.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, club.getName());
+            ps.setString(2, club.getDateFoundation());
+            ps.setInt(3, club.getTrainer().getIdTrainer());
+            ps.executeUpdate();
+
+            try (ResultSet rs = ps.getGeneratedKeys()){
+                if(rs.next()) {
+                    club.setIdClub(rs.getInt(1));
+                }
+            }
+        }
+        return club;
+    }
 }
